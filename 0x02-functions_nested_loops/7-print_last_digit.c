60c1ac9816c0b91b7ff9953cab084b034e9516d6
choukriabdel <choukriabdelhakim7@gmail.com>
2023-07-18 15:49:46 -0700

    0x02-functions_nested_loops

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -11,9 +11,10 @@ int print_last_digit(int n)
 	int last_digt;
 
 	if (n < 0)
-		last_digt = (n * -1) % 10;
+		last_digt = -1 * (n % 10);
 	else
 		last_digt = n % 10;
+
 	_putchar(last_digt + '0');
 	return (last_digt);
 }
